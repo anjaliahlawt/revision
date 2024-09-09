14db0b2ad38ab4bc4632a4f58ae6af8cde344dd6
anjali ahlawat <anjaliahlawat45@gmail.com>
2024-09-09 09:14:37 +0530

    inital commit

---
+++ pattern/p10.java
@@ -0,0 +1,24 @@
+public class p10 
+{
+    public static void main(String[] args) 
+    {
+        int n=5;
+        for(int i=1;i<n;i++)
+        {
+            for(int j=0;j<i;j++)
+            {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+        for(int i=1;i<=n;i++)
+        {
+            for(int j=n;j>=i;j--)
+            {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+    
+}
